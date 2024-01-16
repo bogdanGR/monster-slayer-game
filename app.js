1761bc566b90325054d2b560c5ffbc6f3aa90bf6
bogdanGR <fant1k450@gmail.com>
2024-01-16 08:13:12 +0200

    update the health bar

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function getRandomValue(min, max) {
-    Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
@@ -9,6 +9,14 @@ const app = Vue.createApp({
             monsterHealth: 100,
 
         }
+    },
+    computed: {
+      monsterBarStyles() {
+          return {width: this.monsterHealth + '%'}
+      },
+      playerBarStyles() {
+          return {width: this.playerHealth + '%'}
+      }
     },
     methods: {
         attackMonster() {
